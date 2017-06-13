dc5e9fe0fcfbd8027cd2fe95f09243370d558631
Sherafgan <kssfortis@gmail.com>
2017-06-13 18:04:57 +0300

    refactoring

--- src/main/java/me/sherafgan/springbootstarter/course/CourseRepository.java
+++ src/main/java/me/sherafgan/springbootstarter/course/CourseRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 /**
  * @author Sherafgan Kandov (sherafgan.kandov@gmail.com)
- * @version 6/13/17
  */
 public interface CourseRepository extends CrudRepository<Course, String> {
     public List<Course> findByTopicId(String topicId);
--- src/main/java/me/sherafgan/springbootstarter/topic/TopicRepository.java
+++ src/main/java/me/sherafgan/springbootstarter/topic/TopicRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 /**
  * @author Sherafgan Kandov (sherafgan.kandov@gmail.com)
- * @version 6/13/17
  */
 public interface TopicRepository extends CrudRepository<Topic, String> {
 }
